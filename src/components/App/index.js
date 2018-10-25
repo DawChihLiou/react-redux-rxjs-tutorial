a5c7ad616408ef3803e778abb5b2cb610ae8ac96
Daw-Chih Liou <dawochih.liou@gmail.com>
2018-10-25 10:47:58 +0200

    add BookList component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import './styles.css';
 
 import Navbar from '../Navbar';
 import Home from '../Home';
+import BookList from '../BookList';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Home />
+        <BookList />
       </div>
     );
   }
