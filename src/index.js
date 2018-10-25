38d1cd257220d7de31dde5a9221ed9b6dd953428
Daw-Chih Liou <dawochih.liou@gmail.com>
2018-10-25 12:06:15 +0200

    reset project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+const root = document.querySelector('#root');
 
-import 'bulma/bulma.sass';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+root.innerHTML = 'welcome to the workshop!';
